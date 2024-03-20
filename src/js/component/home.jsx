789336940be6c83570e8aa599119afae03890388
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-03-20 19:49:46 +0000

    texto se borra al hacer enter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = useState ("");
+	const [todos, setTodos] = useState ([]);
+	const [mouseHover, setMouseHover] = useState(null);
+
+	const addTask = (e) => {
+		if (e.key === "Enter") {
+			setTodos(todos.concat([inputValue]));
+			setInputValue("");
+			setMouseHover(null)
+		}
+	};
+
+	const deleteTask = (index) => {
+		const newList = todos.filter((_, i) => i !== index);
+		setTodos(newList);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+		<div className="container-fluid text-center col-6 contenedor">
+			<h1 class="display-3">My todos </h1>
+			<input type="text" className="input form-control" value={inputValue} placeholder="What needs to be done?"
+					onKeyDown={addTask} onChange={(e) => { setInputValue(e.target.value) }} />
+				<ul className="todo-list">
+					{todos.map((todos, index) => (
+						<li className="task" key={index} onMouseEnter={() => setMouseHover(index)}
+							onMouseLeave={() => setMouseHover(null)}>
+							{todos}
+							{mouseHover === index && (
+								<span
+									style={{ cursor: "pointer" }}
+									onClick={() => deleteTask(index)}
+								>
+									&#10006;
+									
+								</span>
+							)}
+						</li>
+					))}
+				</ul>
+			<div className="fw-light" >{todos.length == 0 ? 'No tasks, add a task' : todos.length + ' tasks left'}</div>
 		</div>
+		
 	);
 };
 
