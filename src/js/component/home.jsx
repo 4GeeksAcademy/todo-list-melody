60e6d540f9848aae73c50f678d0f8eddc8824aee
Melody Callejas <154374405+melodycious@users.noreply.github.com>
2024-03-26 20:27:09 +0000

    boton borra, pero no actualiza las tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component 
 const Home = () => {
 
@@ -10,36 +7,50 @@ const Home = () => {
 	const [todos, setTodos] = useState ([]);
 	const [mouseHover, setMouseHover] = useState(null);
 
+	useEffect(() => {
+          getInfo();
+    }, []);
+
+	useEffect(() => {
+		if (todos.length > 0) {
+		  updateInfo(); 
+		}
+	  }, [todos]);
+
+	  const handleSubmit = () => {
+		setTodos(todos.concat({ label: inputValue, done: false }));
+		setInputValue("");
+	  };
+	
+
 	const getInfo = () => {
-		fetch ('https://playground.4geeks.com/apis/fake/todos/user/melodycn', {
-			method: "GET",
-		  })
-		  .then(resp => {
-			  console.log(resp.ok); // Será true si la respuesta es exitosa
-			  console.log(resp.status); // El código de estado 200, 300, 400, etc.
-			  if (resp.status === 404){
-				console.log("creando nuevo usuario")
-				createUser() } ;
-			  console.log(resp.text()); // Intentará devolver el resultado exacto como string
-			  return resp.json() // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-		  })
-		  .then(data => {
-			  // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-			  console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-		  })
-		  .catch(error => {
-			  // Manejo de errores
-			  console.log(error);
-		  });
+		fetch('https://playground.4geeks.com/apis/fake/todos/user/melodycn')
+			.then(resp => {
+				
+				if (resp.status === 404) {
+					console.log("creando nuevo usuario");
+					createUser();
+				}
+				return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+			})
+			.then(result => {
+				// Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				console.log(result); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch(error => {
+				// Manejo de errores
+				console.log(error);
+				})
+			;
 	
 	const createUser = () => {
 		fetch('https://playground.4geeks.com/apis/fake/todos/user/melodycn', {
-      method: "POST",
-      body: JSON.stringify([]),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify([])
+		})
     .then(resp => {
         console.log(resp.ok); // Será true si la respuesta es exitosa
         console.log(resp.status); // El código de estado 200, 300, 400, etc.
@@ -57,69 +68,32 @@ const Home = () => {
 
 	const updateInfo = () =>{
 		fetch('https://playground.4geeks.com/apis/fake/todos/user/melodycn', {
-			method: "POST",
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(todos),
-			headers: {
-			  "Content-Type": "application/json"
-			}
 		  })
-		  .then(resp => {
-			  // console.log(resp.ok); // will be true if the response is successfull
-			  // console.log(resp.status); // the status code = 200 or code = 400 etc.
-			  // console.log(resp.text()); // will try return the exact result as string
-			  return resp.json();        
-		  })
-		  .then(data => {
-			  //here is where your code should start after the fetch finishes
-			  console.log(data); //this will print on the console the exact object received from the server
-		  })
-		  .catch(error => {
-			  //error handling
-			  console.log(error);
-		  });
-		
-	  };
+			.then((response) => response.json())
+			.then((result) => console.log(result))
+			.catch((error) => console.error(error));
+		};
+
 	  const deleteInfo = () => {
 		fetch('https://playground.4geeks.com/apis/fake/todos/user/melodycn', {
 			method: "DELETE",
-			body: JSON.stringify(todos),
-			headers: {
-			  "Content-Type": "application/json"
-			}
 		  })
-		  .then(resp => {
-			  // console.log(resp.ok); // will be true if the response is successfull
-			  console.log(resp.status); 
-			  if (resp.status === 200){
-				console.log("creando nuevo usuario")
-				createUser()
-				setTodos([])};// the status code = 200 or code = 400 etc.
-			  // console.log(resp.text()); // will try return the exact result as string
-					 
-		  })
-		  .then(data => {
-			  //here is where your code should start after the fetch finishes
-			  console.log(data); //this will print on the console the exact object received from the server
-		  })
-		  .catch(error => {
-			  //error handling
-			  console.log(error);
-		  });
+		  .then(response => response.json())
+						.then(data => {
+							console.log(data);
+							window.location.reload();
+						})
+						.catch(error => console.log('Error: ', error));
 		  
 		
 	  };
 
-	useEffect( () => {     //useEffect con funcion dentro que haga un fetch GET de la info del server----- POST para crear nuevo usuario
-		getInfo();
-	  }, [])
-	
-	  useEffect( () => {
-		updateInfo();
-		}, [todos])
-	  
-
 		const addTask = (e) => {
 			if (e.key === "Enter") {
+				handleSubmit();
 				setTodos(todos.concat([inputValue]));
 				setInputValue("");
 				setMouseHover(null)
@@ -129,17 +103,24 @@ const Home = () => {
 		const deleteTask = (index) => {
 			const newList = todos.filter((_, i) => i !== index);
 			setTodos(newList);
-		};
+		}
 		
 	return (
 
 		<div className="container-fluid text-center col-6 contenedor">
 			<h1 className="display-3">My todos </h1>
-			<input type="text" className="input form-control" value={inputValue} placeholder="What needs to be done?"
-					onKeyDown={addTask} onChange={(e) => { setInputValue(e.target.value) }} />
+			<input type="text" 
+				   className="input form-control" 
+				   value={inputValue} 
+				   placeholder="What needs to be done?"
+					onKeyDown={addTask} 
+					onChange={(e) => { setInputValue(e.target.value)  }} 
+					/>
 				<ul className="todo-list">
 					{todos.map((todos, index) => (
-						<li className="task" key={index} onMouseEnter={() => setMouseHover(index)}
+						<li className="task" 
+							key={index} 
+							onMouseEnter={() => setMouseHover(index)}
 							onMouseLeave={() => setMouseHover(null)}>
 							{todos} 
 							{mouseHover === index && (
